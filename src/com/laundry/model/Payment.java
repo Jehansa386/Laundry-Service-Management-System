6929f35b41ef4b74aa4794ed4418d6ef70e8110b
Nidula Yasanjith <nidulayasanjith11@gmail.com>
2026-07-26 21:55:55 +0530

    update payment.java

--- src/com/laundry/model/Payment.java
+++ src/com/laundry/model/Payment.java
@@ -18,3 +18,43 @@ public class Payment {
         this.paymentMethod = paymentMethod;
     }
 
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
